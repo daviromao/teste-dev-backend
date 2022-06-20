7bde877807f24e690d3dfa6e964a6955be0446c8
daviromao <davi.romao0210@gmail.com>
2022-06-20 20:07:31 -0300

    add score calculation test

--- clinic/tests.py
+++ clinic/tests.py
@@ -2,10 +2,13 @@ from django.urls import reverse
 
 from rest_framework import status
 from rest_framework.test import APITestCase
+from django.test import SimpleTestCase
 
 from datetime import date
 
 from clinic.models import Client
+from clinic.utils import calculate_score_from_health_problems
+
 
 class ClientTests(APITestCase):
 
@@ -32,3 +35,21 @@ class ClientTests(APITestCase):
         self.assertEqual(Client.objects.get().name, 'Fulano')
         self.assertEqual(Client.objects.get().health_problems.count(), 3)
 
+class ScoreTests(SimpleTestCase):
+
+    def test_calculate_without_health_problems(self):
+        score = calculate_score_from_health_problems()
+
+        self.assertEqual(score, 5.732417589886876)
+
+    def test_calculate_with_many_health_problems(self):
+        health_problems = [
+            {'name':'diabetes', 'degree': 1},
+            {'name':'diabetes', 'degree': 2},
+            {'name':'parkinson', 'degree': 2},
+        ]
+
+        score = calculate_score_from_health_problems(health_problems)
+
+        self.assertEqual(score, 90.02495108803149)
+        
