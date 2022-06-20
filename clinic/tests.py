e52421c40f1509eca993703d9ab5bc4ce574466c
daviromao <davi.romao0210@gmail.com>
2022-06-20 19:42:43 -0300

    tests: create initial test

--- clinic/tests.py
+++ clinic/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from datetime import date
+
+from clinic.models import Client
+
+class ClientTests(APITestCase):
+
+    def test_post_client(self):
+        url = reverse('client-list')
+
+        health_problems = [
+            {'name':'diabetes', 'degree': 1},
+            {'name':'diabetes', 'degree': 2},
+            {'name':'parkinson', 'degree': 2},
+        ]
+
+        data = {
+            'name': 'Fulano',
+            'birthdate': date(2000, 5, 3),
+            'gender': 'M',
+            'health_problems': health_problems
+        }
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Client.objects.count(), 1)
+        self.assertEqual(Client.objects.get().name, 'Fulano')
+        self.assertEqual(Client.objects.get().health_problems.count(), 3)
+
